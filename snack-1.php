5c160f76e4b57479b44a9ae388df5a20cd2fec43
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-06-01 00:20:00 +0200

    Ex 4 done

--- snack-1.php
+++ snack-1.php
@@ -39,3 +39,6 @@ $terzaGiornata = [
     }
     ?>
 </ul>
+
+
+
---
+++ snack-4.php
@@ -0,0 +1,21 @@
+<!-- ## Snack 4
+Creare un array con 15 numeri casuali, 
+tenendo conto che l’array non dovrà contenere 
+lo stesso numero più di una volta -->
+
+<?php 
+
+$newArr = [];
+$length = 15;
+$min = 0;
+$max = 100;
+
+    while (count($newArr) < $length) {
+        $randomNumber = rand($min, $max);
+
+        if (!in_array($randomNumber, $newArr)) {
+            $newArr[] = $randomNumber;
+        }
+    }
+    var_dump($newArr);
+?>
